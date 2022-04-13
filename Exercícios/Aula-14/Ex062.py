2997e5bc483b2b911541b51ce5045ba1fe88ca16
Rafael Machado <rafa.rafael3@gmail.com>
2022-04-13 20:28:11 -0300

    arquivos de aula

--- Exercícios/Aula-14/Ex062.py
+++ Exercícios/Aula-14/Ex062.py
@@ -1,3 +1,3 @@
-# Melhore o DESAFIO 061, perguntando o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele
+# Melhore o DESAFIO 061, perguntando o utilizador se ele quer mostrar mais alguns termos. O programa encerra quando ele
 # disser que quer mostrar 0 termos.
 
--- Exercícios/Aula-14/Ex064.py
+++ Exercícios/Aula-14/Ex064.py
@@ -1,4 +1,4 @@
-# Crie um programa que leia vários números inteiros pelo telcado. o programa só vai parar quando o usuário digitar o
-# valor 999, que é a condição de parada. No final mostre quaantos números foram digitados e qual foi a soma entre
+# Crie um programa que leia vários números inteiros pelo telcado. o programa só vai parar quando o utilizador digitar o
+# valor 999, sendo a condição de parada. No final mostre quaantos números foram digitados e qual foi a soma entre
 # eles(Desconsiderando o flag).
 
