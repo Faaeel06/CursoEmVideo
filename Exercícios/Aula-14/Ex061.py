71c22a3785e851f0bf44025a6c272c1ce096b605
Rafael Machado <79583259+Faaeel06@users.noreply.github.com>
2022-10-29 21:06:44 -0300

    Final de módulo

--- Exercícios/Aula-14/Ex061.py
+++ Exercícios/Aula-14/Ex061.py
@@ -1,17 +1,3 @@
 # Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
-# usando a estrutura WHILE.
-
-contador = 0
-inicio = int(input('Digite o Primeiro Termo: '))
-razao = int(input('Digite a Razão: '))
-
-# for contador in range(inicio, inicio + 10 * razao, razao):
-#     print(contador)
-
-while True:
-    if contador < (inicio + 10 * razao):
-        contador = inicio + razao
-    else:
-        break
-    print(contador)
+# Usando a estrutura WHILE.
 
